0caedece74000c4037ff4de293977318ae1fcafd
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-28 00:15:43 +0500

    animation-01

---
+++ GSAP-01/main.js
@@ -0,0 +1,29 @@
+// gsap.to('#box1',{
+//     x: 900,
+//     duration: 2,
+//     delay: 1
+// })
+
+
+// gsap.to('#box2',{
+//     x: 900,
+//     y: 500,
+//     scale: 0.5,
+//     duration: 2,
+//     delay: 1
+// })
+
+gsap.to('#box1', {
+    x:1200,
+    duration: 2, 
+    delay: 1,
+    backgroundColor: 'green',
+    rotate: 360,
+    borderRadius: "50%"
+})
+// gsap.from('#box1', {
+//     x:1200,
+//     duration: 2, 
+//     delay: 1,
+//     backgroundColor: 'green'
+// })
